18c11848fadaacdbe99fcdad67ab9d7a5cb89bb6
bdrtelco <tic-tele@banrural.com.gt>
2018-01-03 06:25:08 -0600

    daily autocommit

--- procs/carga_trans/sybsql.sql
+++ procs/carga_trans/sybsql.sql
@@ -5,8 +5,8 @@ ce_oficina, ",", ce_region, ",",    datepart (yy, ce_fecha ),  ",", datepart (mm
 convert(money,sum(ce_efectivo + ce_chq_propios  + ce_chq_locales  +  ce_chq_ot_plaza)) monto, ");"
 from cob_datawarehouse..cj_caja_estadistico_his
 where ce_oficina < 2000
-and ce_fecha >= '11/01/2017'
-and ce_fecha < '12/01/2017'
+and ce_fecha >= '12/01/2017'
+and ce_fecha < '01/01/2018'
 and ce_moneda >= 0
 and ce_transaccion >= 0
 and ce_operador not in ('sa', 'reentry')
--- procs/gensla/update-caida.sql
+++ procs/gensla/update-caida.sql
@@ -1,7 +1,7 @@
 update enlace_caida 
  set ec_duracion = nagios.nagtime(ec_fecha_ini, ec_fecha_fin, 0), 
   ec_duracion_formato=nagios.nagtime(ec_fecha_ini, ec_fecha_fin, 1) 
- where ec_fecha_ini > '2017-11-01 00:00:00' 
+ where ec_fecha_ini > '2017-12-01 00:00:00' 
  and year(ec_fecha_ini)=2017 
- and month(ec_fecha_ini) = 11; 
+ and month(ec_fecha_ini) = 12; 
  

c647e3e60cdc7ad21b50a1304272735b605a8695
bdrtelco <tic-tele@banrural.com.gt>
2018-04-01 06:25:06 -0600

    daily autocommit

--- procs/gensla/tmp-.sql
+++ procs/gensla/tmp-.sql
@@ -268,3 +268,13 @@ and en_tipo_enlace='PRI'
 and rm_banrural =1 
 and  sl_anio =2011 
 and sl_mes = 11; 
+ SELECT en_proveedor, en_remoto, en_estado, en_nagios, rm_descripcion, rm_region, en_codigo_proveedor, en_fecha_activacion, 
+sl_uptime, sl_downtime, sl_unreachable, sl_undetermined,  en_costo_mensual 
+FROM enlaces.enlace r , nagios_stats.sladata, enlaces.remoto 
+where r.en_nagios = nagios_stats.sladata.sl_host 
+and en_remoto = rm_remoto 
+and en_estado = 7 
+and en_tipo_enlace='PRI' 
+and rm_banrural =1 
+and  sl_anio =2011 
+and sl_mes = 11; 
